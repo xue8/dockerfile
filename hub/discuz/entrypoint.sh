b51836e5787062f4e9cc15bb5647690c786d5cf9
xue8 <rongyuexue@gmail.com>
2021-11-14 15:16:33 +0800

    add discuz dockerfile

---
+++ hub/discuz/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ set -eux
+ 
+ /etc/init.d/php7.4-fpm start
+ /etc/init.d/nginx start
+
+ mkdir -p /data/www/web
+ chmod -R 777 /data/www
+ mv /tmp/*  /data/www/web
+
+tail -f  /etc/hosts
